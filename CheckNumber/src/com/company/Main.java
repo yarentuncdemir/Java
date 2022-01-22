5bd24a19a98b211db4cdd37752394500c9570173
Yaren Tunçdemir <49490879+yarentuncdemir@users.noreply.github.com>
2022-01-22 16:30:39 +0300

    Add files via upload

---
+++ CheckNumber/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        checkNumber(6);
+
+    }
+    public static void checkNumber(int number) {
+        if(number>0){
+            System.out.println("positive");
+
+        }
+        else if(number<0){
+            System.out.println("negative");
+
+        }
+        else
+            System.out.println("ıts zero")
+    }
+
+}
