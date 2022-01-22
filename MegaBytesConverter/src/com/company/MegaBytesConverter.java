96809e52c1f3cafb96b5509ef09ff2c7d06c8821
Yaren Tunçdemir <49490879+yarentuncdemir@users.noreply.github.com>
2022-01-22 16:40:39 +0300

    Add files via upload

---
+++ MegaBytesConverter/src/com/company/MegaBytesConverter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes (int kiloBytes){
+        if(kiloBytes <0){
+            System.out.println("Invalid Value");
+        }else{
+            int convertBytes = kiloBytes;
+            int megaByte = (convertBytes / 1024);
+            int remainingkiloByte = (convertBytes % 1024);
+            System.out.println(convertBytes + " KB = " + megaByte + " MB and " + remainingkiloByte + " KB");
+        }
+    }
+}
